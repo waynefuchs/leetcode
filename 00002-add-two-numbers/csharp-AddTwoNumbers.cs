91b8986ed8888d4f8864c698e83cbec44f4cc06a
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-05-29 16:27:32 -0400

    Refactor Add Two Numbers
    Leetcode says that my refactor is a lot slower. I can't see how I'm
    losing over 30ms to 4 null checks. I submitted my code multiple times
    without change, and got between 128ms all the way up to 183ms. It's kind
    of annoying. I understand why this happens, but it is still annoying
    that I can gauge the performance.

--- 00002-add-two-numbers/csharp-AddTwoNumbers.cs
+++ 00002-add-two-numbers/csharp-AddTwoNumbers.cs
@@ -13,48 +13,25 @@ public class Solution
 {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        ListNode firstNode = new ListNode(-1);
-        ListNode currentNode = firstNode;
+        ListNode headNode = new ListNode(int.MinValue);
+        ListNode currentNode = headNode;
         int carry = 0;
 
-        // Both l1 and l2 have data
-        while (l1 != null && l2 != null)
+        while (l1 != null || l2 != null)
         {
-            int sum = l1.val + l2.val + carry;
+            int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
             int onesPlace = sum % 10;
             carry = sum / 10;
             currentNode.next = new ListNode(onesPlace);
             currentNode = currentNode.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        // l1 has data, l2 is null
-        while (l1 != null)
-        {
-            int sum = l1.val + carry;
-            int onesPlace = sum % 10;
-            carry = sum / 10;
-            currentNode.next = new ListNode(onesPlace);
-            currentNode = currentNode.next;
-            l1 = l1.next;
-        }
-
-        // l2 has data, l1 is null
-        while (l2 != null)
-        {
-            int sum = l2.val + carry;
-            int onesPlace = sum % 10;
-            carry = sum / 10;
-            currentNode.next = new ListNode(onesPlace);
-            currentNode = currentNode.next;
-            l2 = l2.next;
+            l1 = l1?.next;
+            l2 = l2?.next;
         }
 
         // Take care of remaining carry
         if (carry > 0) currentNode.next = new ListNode(carry);
 
-        return firstNode.next;
+        return headNode.next;
     }
 
 }
