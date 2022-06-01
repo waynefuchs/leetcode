c91f1dbeb70c0430d708511a2ffc2c7362747ae7
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-01 09:32:03 -0400

    Refactor and remove unnecessary variables

--- 01480-running-sum-of-1d-array/csharp-RunningSumOf1dArray.cs
+++ 01480-running-sum-of-1d-array/csharp-RunningSumOf1dArray.cs
@@ -1,11 +1,7 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
-        int[] runningSums = new int[nums.Length];
-        int sum = 0;
-        for(int i=0; i<nums.Length; i++) {
-            sum += nums[i];
-            runningSums[i] = sum;
-        }
-        return runningSums;
+        for(int i=1; i<nums.Length; i++) 
+            nums[i] += nums[i-1];
+        return nums;
     }
 }
