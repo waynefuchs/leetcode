3c03895ee1d30eb749a6fab9f276267a0c307326
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-03 19:39:46 -0400

    Implement Rolling Sum Solution
    This one passes test cases before timeout, but it's still pretty bad.
    In the discussion there's apparently an O(1) solution that I'm going
    to attempt to implement before calling it quits on this one.

--- 00304-range-sum-query-2d-immutable/csharp-RangeSumeQuery2DImmutable.cs
+++ 00304-range-sum-query-2d-immutable/csharp-RangeSumeQuery2DImmutable.cs
@@ -1,15 +1,19 @@
 public class NumMatrix {
 
     private readonly int[][] _matrix;
-    public NumMatrix(int[][] matrix) {
+    public NumMatrix(int[][] matrix) 
+    {
+        // Store the running sum of each row as the new matrix
+        for(int row=0; row<matrix.Length; row++)
+            for(int col=1; col<matrix[0].Length; col++)
+                matrix[row][col] += matrix[row][col-1];
         _matrix = matrix;
     }
-    
+
     public int SumRegion(int rowStart, int colStart, int rowEnd, int colEnd) {
         int sum = 0;
-        for(int col=colStart; col<=colEnd; col++) 
-            for(int row=rowStart; row<=rowEnd; row++)
-                sum += _matrix[row][col];
+        for(int row=rowStart; row<=rowEnd; row++)
+            sum += _matrix[row][colEnd] - (colStart>0 ? _matrix[row][colStart-1] : 0);
         return sum;
     }
 }
