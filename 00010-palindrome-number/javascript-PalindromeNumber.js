cfb58e30b3c397b0073937b68d5a6eff83c3f5e5
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-01 10:10:15 -0500

    Rewrite solution to include follow up
    The follow up states that conversion to string should not be used.

--- 00010-palindrome-number/javascript-PalindromeNumber.js
+++ 00010-palindrome-number/javascript-PalindromeNumber.js
@@ -2,6 +2,17 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    return Number(x.toString().split('').reverse().join('')) === Number(x);
+var isPalindrome = function (x) {
+  // Conversion to string is not allowed by the 'Follow-Up'
+  // return Number(x.toString().split('').reverse().join('')) === Number(x);
+  if (x < 0) return false;
+  if (x % 10 == 0 && x != 0) return false;
+
+  let revertedNumber = 0;
+  while (x > revertedNumber) {
+    revertedNumber = (revertedNumber * 10 + (x % 10)) | 0;
+    x = (x / 10) | 0;
+  }
+
+  return revertedNumber == x || ((revertedNumber / 10) | 0) == x;
 };
