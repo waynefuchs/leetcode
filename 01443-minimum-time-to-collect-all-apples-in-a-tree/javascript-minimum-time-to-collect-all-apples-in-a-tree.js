a122d290074d2df87a15a26ddf418bd4f67815c3
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-13 22:28:12 -0500

    Working algorithm
    I am trying to understand how this works... And why mine doesn't.

--- 01443-minimum-time-to-collect-all-apples-in-a-tree/javascript-minimum-time-to-collect-all-apples-in-a-tree.js
+++ 01443-minimum-time-to-collect-all-apples-in-a-tree/javascript-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -10,30 +10,49 @@
  * @param {boolean[]} hasApple
  * @return {number}
  */
-var minTime = function (n, edges, hasApple) {
-  // Build the tree
-  let tree = {};
-  for (const [parent, child] of edges) {
-    if (!tree[child])
-      tree[child] = { parent: null, traveled: false, hasApple: false };
-    tree[child].parent = parent;
-    tree[child].hasApple = hasApple[child];
-  }
+// var minTime = function (n, edges, hasApple) {
+//   // Build the tree
+//   let tree = {};
+//   for (const [parent, child] of edges) {
+//     if (!tree[child])
+//       tree[child] = { parent: null, traveled: false, hasApple: false };
+//     tree[child].parent = parent;
+//     tree[child].hasApple = hasApple[child];
+//   }
 
-  // Traverse the tree starting from every tree that has apples, and working back to the root node
-  // If I encounter a node I've already traveled, I go to the next tree with apples
-  let apples = Object.keys(tree).filter((i) => tree[i].hasApple);
-  for (let i of apples) {
-    while (i in tree) {
-      if (tree[i].traveled) break;
-      tree[i].traveled = true;
-      i = tree[i].parent;
+//   // Traverse the tree starting from every tree that has apples, and working back to the root node
+//   // If I encounter a node I've already traveled, I go to the next tree with apples
+//   let apples = Object.keys(tree).filter((i) => tree[i].hasApple);
+//   for (let i of apples) {
+//     while (i in tree) {
+//       if (tree[i].traveled) break;
+//       tree[i].traveled = true;
+//       i = tree[i].parent;
+//     }
+//   }
+
+//   // Count all the nodes that were traveled
+//   let walked = Object.keys(tree).filter((i) => tree[i].traveled);
+//   return walked.length * 2;
+// };
+
+var minTime = function (n, edges, hasApple) {
+  let map = {};
+  for (const [x, y] of edges) map[y] = x;
+
+  let res = 0;
+  let check = {};
+  for (let i = 1; i < hasApple.length; i++) {
+    if (hasApple[i]) {
+      let curV = i;
+      while (curV != 0 && !check[curV]) {
+        res += 2;
+        check[curV] = true;
+        curV = map[curV];
+      }
     }
   }
-
-  // Count all the nodes that were traveled
-  let walked = Object.keys(tree).filter((i) => tree[i].traveled);
-  return walked.length * 2;
+  return res;
 };
 
 // n = 7;
