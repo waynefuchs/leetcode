e44be2d96f049fec20f74681a1e397d5e20677b7
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-15 13:07:35 -0500

    Removed array dependence

--- 00008-String-to-integer-(atoi)/javascript-string-to-integer-(atoi).js
+++ 00008-String-to-integer-(atoi)/javascript-string-to-integer-(atoi).js
@@ -5,7 +5,6 @@
 var myAtoi = function (s) {
   let index = 0;
   let positive = true;
-  let digitArray = [];
   let result = 0;
 
   // 1. Read in and ignore any leading whitespace.
@@ -22,27 +21,31 @@ var myAtoi = function (s) {
 
   // 3. Read in next the characters until the next non-digit character or the end of the input is reached.
   //    The rest of the string is ignored.
-  for (; index < s.length; index++) {
-    const code = s.charCodeAt(index);
-    if (code < 48 || code > 57) break;
-    digitArray.unshift(code - 48);
-  }
-
+  //
+  // Steps 3 & 4 can be combined
+  //
   // 4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32).
   //    If no digits were read, then the integer is 0.
   //    Change the sign as necessary (from step 2).
-  for (let power = 0; power < digitArray.length; power++) {
-    result += digitArray[power] * 10 ** power;
+  for (; index < s.length; index++) {
+    // shift ascii offset by 48 ("0")
+    const code = s.charCodeAt(index) - 48;
+    // end conversion if a non-number character is found
+    if (code < 0 || code > 9) break;
+    // perform step 4
+    result *= 10;
+    result += code;
   }
 
   // 5. If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range.
   //    Specifically, integers less than -2^31 should be clamped to -2^31, and integers greater than 231 - 1 should be clamped to 231 - 1.
-  if (result > 2147483648) return positive ? 2147483648 : -2147483648;
+  if (positive ? result > 2147483647 : result > 2147483648)
+    return positive ? 2147483647 : -2147483648;
 
   // 6. Return the integer as the final result.
   if (result === 0) return 0;
   return positive ? result : -result;
 };
 
-n = myAtoi("                43 with words!");
+n = myAtoi("               -123443 with words!");
 console.log(n);
