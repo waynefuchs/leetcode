c0d6672f1f446a92f0d6bc6ece3a917cea45a333
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-15 00:32:31 -0500

    Added solution for 1443

--- 01443-minimum-time-to-collect-all-apples-in-a-tree/javascript-minimum-time-to-collect-all-apples-in-a-tree.js
+++ 01443-minimum-time-to-collect-all-apples-in-a-tree/javascript-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -1,77 +1,42 @@
-// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/description/
-// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/solutions/2864715/minimum-time-to-collect-all-apples-in-a-tree/
-// https://www.youtube.com/watch?v=-A6hkNg7FN0
-
-// The following code does not take bi-directionality into consideration (error)
-
+// Copied from:
+// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/solutions/3033321/javascript-beats-100/
+//
+// The problem description is not complete
 /**
  * @param {number} n
  * @param {number[][]} edges
  * @param {boolean[]} hasApple
  * @return {number}
  */
-// var minTime = function (n, edges, hasApple) {
-//   // Build the tree
-//   let tree = {};
-//   for (const [parent, child] of edges) {
-//     if (!tree[child])
-//       tree[child] = { parent: null, traveled: false, hasApple: false };
-//     tree[child].parent = parent;
-//     tree[child].hasApple = hasApple[child];
-//   }
+function minTime(n, edges, hasApple) {
+  // This checks if there are no apples in the tree return 0
+  if (edges.filter((v) => v).length === 0) return 0;
 
-//   // Traverse the tree starting from every tree that has apples, and working back to the root node
-//   // If I encounter a node I've already traveled, I go to the next tree with apples
-//   let apples = Object.keys(tree).filter((i) => tree[i].hasApple);
-//   for (let i of apples) {
-//     while (i in tree) {
-//       if (tree[i].traveled) break;
-//       tree[i].traveled = true;
-//       i = tree[i].parent;
-//     }
-//   }
+  // Create an array of arrays that is 'n' in length
+  const adjList = new Array(n);
+  for (let i = 0; i < n; i++) adjList[i] = [];
+  // Create an adjacency list to represent the tree
+  for (const [a, b] of edges) {
+    adjList[a].push(b);
+    adjList[b].push(a);
+  }
 
-//   // Count all the nodes that were traveled
-//   let walked = Object.keys(tree).filter((i) => tree[i].traveled);
-//   return walked.length * 2;
-// };
-
-var minTime = function (n, edges, hasApple) {
-  let map = {};
-  for (const [x, y] of edges) map[y] = x;
-
-  let res = 0;
-  let check = {};
-  for (let i = 1; i < hasApple.length; i++) {
-    if (hasApple[i]) {
-      let curV = i;
-      while (curV != 0 && !check[curV]) {
-        res += 2;
-        check[curV] = true;
-        curV = map[curV];
+  // Helper function to dfs through the tree
+  // (depth first search)
+  function dfs(node, parent) {
+    let time = 0;
+    for (const child of adjList[node]) {
+      if (child !== parent) {
+        time += dfs(child, node);
       }
     }
+    // If the current node has an apple or if one of its children has an apple, we need to visit this node
+    if (hasApple[node] || time > 0) {
+      time += 2;
+    }
+    return time;
   }
-  return res;
-};
 
-// n = 7;
-// edges = [
-//   [0, 1],
-//   [0, 2],
-//   [1, 4],
-//   [1, 5],
-//   [2, 3],
-//   [2, 6],
-// ];
-// hasApple = [false, false, true, false, false, true, false];
-
-n = 4;
-edges = [
-  [0, 2],
-  [0, 3],
-  [1, 2],
-];
-hasApple = [false, true, false, false];
-let res = minTime(n, edges, hasApple);
-console.dir(res);
+  // Start the dfs at the root node (vertex 0)
+  return dfs(0, -1) - 2;
+}
