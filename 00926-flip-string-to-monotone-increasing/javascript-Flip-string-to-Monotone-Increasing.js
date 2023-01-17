da6f1ea0af6959da6647b996d6a2f7754773f468
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-01-17 13:00:47 -0500

    Solution for 926

--- 00926-flip-string-to-monotone-increasing/javascript-Flip-string-to-Monotone-Increasing.js
+++ 00926-flip-string-to-monotone-increasing/javascript-Flip-string-to-Monotone-Increasing.js
@@ -2,6 +2,34 @@
  * @param {string} s
  * @return {number}
  */
-var minFlipsMonoIncr = function(s) {
-
+var minFlipsMonoIncr = function (s) {
+  let m = s.split("").filter((v) => v === "0").length;
+  let ans = m;
+  for (const c of s) {
+    if (c === "0") ans = Math.min(ans, --m);
+    else ++m;
+  }
+  return ans;
 };
+
+let result;
+//Example 1:
+// Input: s = "00110"
+// Output: 1
+// Explanation: We flip the last digit to get 00111.
+result = minFlipsMonoIncr("00110");
+console.log(`Example1 result: ${result}`);
+
+// Example 2:
+// Input: s = "010110"
+// Output: 2
+// Explanation: We flip to get 011111, or alternatively 000111.
+result = minFlipsMonoIncr("010110");
+console.log(`Example2 result: ${result}`);
+
+// Example 3:
+// Input: s = "00011000"
+// Output: 2
+// Explanation: We flip to get 00000000.
+result = minFlipsMonoIncr("00110000");
+console.log(`Example3 result: ${result}`);
